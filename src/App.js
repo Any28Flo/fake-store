89f51895bdebf1528bf12513c4412ac91cabd7b5
Erika <foglzerika@gmail.com>
2021-04-23 06:55:23 -0500

    feat | Create a list of products

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
+import React, {useState,useEffect} from 'react';
+import { Layout, Row, Col, Card } from 'antd';
+import 'antd/dist/antd.css';
 import './App.css';
+
+const {  Content} = Layout;
+const { Meta } = Card;
 
 const App =()=> {
   //  const URL =
-  
+  const [products, setProducts] = useState([]);
+
+
+  useEffect(()=>{
+      fetch('https://fakestoreapi.com/products?limit=5')
+          .then(res=>res.json())
+          .then(json=>{
+                 setProducts(json)
+          })
+  }, [])
+
   return (
-    <div className="App">
-      <h2>hola</h2>
-    </div>
+      <Layout>
+          <Content style={{ padding: '0 50px' }}>
+              <Row gutter={16}>
+                      {
+                          products.map(product =>{
+                              console.log(product)
+                              return(
+                                  <Col  lg={6}>
+                                      <Card
+                                          key={product.id}
+                                          hoverable
+                                          className="card-product"
+                                          cover={<img alt="example" src={product.image} className="card-img" />}
+                                      >
+                                          <Meta title={product.title} description={product.description} />
+                                      </Card>
+                                  </Col>
+                              )
+                          })
+                      }
+              </Row>
+          </Content>
+
+      </Layout>
+
   );
 }
 
