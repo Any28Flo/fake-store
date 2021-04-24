36a9c81f2c229f68d7f08effe1e44b2d3e048558
Erika <foglzerika@gmail.com>
2021-04-23 21:31:14 -0500

    feat | Agregando react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom';
 import 'antd/dist/antd.css';
 import './App.css';
 
-
+import ListProducts from "./pages/ListProducts/ListProducts";
+import DetailProduct from "./pages/DetailProduct/DetailProduct";
 
 const App =()=> {
+    return(
+        <Switch>
+            <Route exact path="/">
+                <ListProducts/>
+            </Route>
+            <Route path="/product/:id">
+                <DetailProduct/>
+            </Route>
 
+        </Switch>
+    )
 }
 
 export default App;
--- src/pages/ListProducts/ListProducts.js
+++ src/pages/ListProducts/ListProducts.js
@@ -1,6 +1,7 @@
 import React , {useState,useEffect} from 'react';
 
 import {Card, Col, Layout, Row} from "antd";
+import {Link} from "react-router-dom";
 
 import getProducts from "../../services/getProducts";
 
@@ -26,15 +27,17 @@ const ListProducts = () =>{
                         products.map(product =>{
                             console.log(product)
                             return(
-                                <Col  lg={6}>
-                                    <Card
-                                        key={product.id}
-                                        hoverable
-                                        className="card-product"
-                                        cover={<img alt="example" src={product.image} className="card-img" />}
-                                    >
-                                        <Meta title={product.title} description={product.description} />
-                                    </Card>
+                                <Col  lg={6}> `
+                                    <Link to={`/product/${product.id}` }>
+                                        <Card
+                                            key={product.id}
+                                            hoverable
+                                            className="card-product"
+                                            cover={<img alt="example" src={product.image} className="card-img" />}
+                                        >
+                                            <Meta title={product.title} description={product.description} />
+                                        </Card>
+                                    </Link>
                                 </Col>
                             )
                         })
